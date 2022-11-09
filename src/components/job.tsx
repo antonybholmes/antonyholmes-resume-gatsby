bef80cc7b18f9168119ec4cb81ff83ed46527b9c
Antony Holmes <antony@antonybholmes.com>
2022-11-08 23:57:05 -0500

    Bug fixes and updates.

--- src/components/job.tsx
+++ src/components/job.tsx
@@ -12,11 +12,17 @@ const Job = ({ date, title, place, children }: IProps) => (
   <div className="flex flex-col">
     <Row className="justify-between items-center">
       <div>
-        <h2 className="font-medium text-base leading-tight text-blue-800">{title}</h2>
+        <h2 className="font-medium text-base leading-tight text-blue-800">
+          {title}
+        </h2>
         <Row className="gap-x-2 items-center  text-blue-500">
           <h3 className="leading-tight">{place}</h3>
-          {date && (<><span>|</span>
-          <h3 className="leading-tight">{date}</h3></>)}
+          {date && (
+            <>
+              <span>|</span>
+              <h3 className="leading-tight">{date}</h3>
+            </>
+          )}
         </Row>
       </div>
       {/* <h3 className="text-base font-light leading-tight text-gray-500">{date}</h3> */}
